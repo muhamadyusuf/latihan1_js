1c8fee75195faf6f9aa5e94ef72ed567e0d8c0b3
Muhamad Yusuf <muhamadyusuf0012@gmail.com>
2025-10-21 09:57:45 +0700

    penambahan nilai angka & huruf

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function kirim() {
     let nama_depan = document.getElementById("nama_depan").value;
     let nama_belakang = document.getElementById("nama_belakang").value;
+    let nilai_angka = document.getElementById("nilai_angka").value;
+
+    var nilai_huruf = "";
+    switch(true){
+        case (nilai_angka >= 85):
+            nilai_huruf = "A";
+            break;
+        case (nilai_angka >= 80):
+            nilai_huruf = "A-";
+            break;
+        case (nilai_angka >= 75):
+            nilai_huruf = "B+";
+            break;
+        case (nilai_angka >= 70):
+            nilai_huruf = "B";
+            break;
+        case (nilai_angka >= 65):
+            nilai_huruf = "B-";
+            break;
+        case (nilai_angka >= 60):
+            nilai_huruf = "C+";
+            break;
+        case (nilai_angka >= 55):
+            nilai_huruf = "C";
+            break;
+        case (nilai_angka >= 40):
+            nilai_huruf = "D";
+            break;
+        default:
+            nilai_huruf = "E";
+    }
     console.log("Nama Lengkap: " + nama_depan + " " + nama_belakang);
+    console.log("Nilai Angka: " + nilai_angka);
+    console.log("Nilai Huruf: " + nilai_huruf);
 }
