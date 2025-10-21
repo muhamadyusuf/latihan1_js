fb050a68bbfa1bf2fdb834f96ddbcd12fbd89791
Muhamad Yusuf <muhamadyusuf0012@gmail.com>
2025-10-21 09:44:19 +0700

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+function kirim() {
+    let nama_depan = document.getElementById("nama_depan").value;
+    let nama_belakang = document.getElementById("nama_belakang").value;
+    console.log("Nama Lengkap: " + nama_depan + " " + nama_belakang);
+}
